f09a2771e7a230a558c411305fffab13b2eedfeb
dev_neil_a <neilallwood@icloud.com>
2024-01-02 18:30:01 +0000

    Updated code examples

--- day-001-vars-const/001-var-const.playground/Contents.swift
+++ day-001-vars-const/001-var-const.playground/Contents.swift
@@ -9,3 +9,34 @@ print(greeting)
 
 // This is a constant. Once its value has been set, it cannot be changed:
 let person = "Fred"
+
+// Strings:
+
+// An example of multiline strings:
+let multilineExanple = """
+This is an
+example of a multi-line string
+"""
+
+print(multilineExanple)
+
+// Get the length of a string:
+print(person.count)
+
+// Display a string as uppercase:
+print(person.uppercased())
+
+// Display a string as lowercase:
+print(person.lowercased())
+
+// Check if a string has a particular prefix:
+print(greeting.hasPrefix("Hello"))
+
+// Check if a string has a particular suffix:
+print(greeting.hasSuffix(".jpg"))
+
+// Integers:
+
+
+
+// Decimals:
