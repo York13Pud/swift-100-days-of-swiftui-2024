29e0a1ddb6ab6875f25edde034547973e8f31805
dev_neil_a <neilallwood@icloud.com>
2024-01-02 16:34:00 +0000

    Added day 1 examples

---
+++ day-001-vars-const/001-var-const.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+// This is a variable. The assigned value can be changed:
+var greeting = "Hello, playground"
+print(greeting)
+
+greeting = "Hello, World!"
+print(greeting)
+
+// This is a constant. Once its value has been set, it cannot be changed:
+let person = "Fred"
