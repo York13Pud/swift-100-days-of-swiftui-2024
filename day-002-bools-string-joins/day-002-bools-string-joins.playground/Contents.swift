dc9aefd21b54535215d0bf5a540c291ec9705ee9
dev_neil_a <neilallwood@icloud.com>
2024-01-03 10:54:07 +0000

    Added boolean examples

--- day-002-bools-string-joins/day-002-bools-string-joins.playground/Contents.swift
+++ day-002-bools-string-joins/day-002-bools-string-joins.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Cocoa
 
+// booleans:
 
+// Store a boolean:
+var storedBool = true
+
+// You can use reverse logic with ! on bools:
+var isAuthenticated = false
+
+isAuthenticated = !isAuthenticated
+print(isAuthenticated) // true
+
+isAuthenticated = !isAuthenticated
+print(isAuthenticated) // false
+
+// Another method to switch a boolean is with toggle:
+var gameOver = false
+print(gameOver) // false
+
+gameOver.toggle()
+print(gameOver) // true
+
+print(1 == 2)
