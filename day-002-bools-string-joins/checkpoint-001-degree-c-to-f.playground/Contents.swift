e739baf601e26d6fed8092a336a03484302a211f
dev_neil_a <neilallwood@icloud.com>
2024-01-03 12:25:11 +0000

    Completed checkpoint 1 exercise

---
+++ day-002-bools-string-joins/checkpoint-001-degree-c-to-f.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+// Set a temp in degrees c as a double:
+let tempC:Double = 1.0
+
+// Display temp in c and f:
+print("\(tempC) degrees celcius is \(tempC * 9 / 5 + 32) in fahrenheit")
