c5721d79018d7c0269f65e5aea4ac602f31d8a1a
dev_neil_a <neilallwood@icloud.com>
2024-01-03 14:43:16 +0000

    Added array examples

--- day-003-complex-data-types-pt1/day-003-complex-data-types-pt1.playground/Contents.swift
+++ day-003-complex-data-types-pt1/day-003-complex-data-types-pt1.playground/Contents.swift
@@ -1,3 +1,19 @@
 import Cocoa
 
+// Create an array of strings:
+var names = ["Bob", "Fred", "Frank"]
 
+// Print out the first item in the array:
+print(names[0])
+
+// Add to the array:
+names.append("Brian")
+
+// Print out the items in the array:
+print(names)
+
+// Remove the last item in the array:
+names.remove(at: 3)
+
+// Print out the items in the array:
+print(names)
