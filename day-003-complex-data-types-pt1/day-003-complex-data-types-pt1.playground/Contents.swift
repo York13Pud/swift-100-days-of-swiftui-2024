3675276bfde43b91cf96ec5a27e9dab24e41ab3f
dev_neil_a <neilallwood@icloud.com>
2024-01-04 10:59:51 +0000

    Added array examples

--- day-003-complex-data-types-pt1/day-003-complex-data-types-pt1.playground/Contents.swift
+++ day-003-complex-data-types-pt1/day-003-complex-data-types-pt1.playground/Contents.swift
@@ -17,3 +17,33 @@ names.remove(at: 3)
 
 // Print out the items in the array:
 print(names)
+
+// Show the number of items in the array:
+print(names.count)
+
+// Search the array by value. It will return true or false:
+print(names.contains("Fred"))
+
+// Sort an array. This will not update the order of the items in the array:
+print(names.sorted())
+
+// Reverse an array. This will not update the order of the items in the array:
+print(names.reversed())
+
+print(names)
+
+// Create an empty array that will contain integers.
+// Note: If you don't specify the datatype for the array (<Int> in this case), XCode will not allow it:
+var scores = Array<Int>()
+
+// Add values to the array:
+scores.append(1)
+scores.append(2)
+scores.append(3)
+
+// Print the scores array:
+print(scores)
+
+// Another way to write out an empty array in a more shorthanded way:
+var albums = [String]()
+
