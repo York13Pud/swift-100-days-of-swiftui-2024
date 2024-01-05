31d738167076e3056e8dbc5103ebb18688544f7e
dev_neil_a <neilallwood@icloud.com>
2024-01-05 16:45:47 +0000

    Added switch examples

--- 003-conditions/003-conditions.playground/Contents.swift
+++ 003-conditions/003-conditions.playground/Contents.swift
@@ -37,3 +37,39 @@ if "a" > "B" { // 61 > 42 in hex, 97 vs. 66 in dec
 
 // Switch Statements:
 
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+print(place)
+
+
+// Use fallthrough so that when the value is met, it will carry on through the other cases in the switch:
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
