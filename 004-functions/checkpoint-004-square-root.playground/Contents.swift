a36059887e4d3a663e5ffaf15fe8e0cf46c29c6c
dev_neil_a <neilallwood@icloud.com>
2024-01-09 16:04:28 +0000

    Completed checkpoint 4

--- 004-functions/checkpoint-004-square-root.playground/Contents.swift
+++ 004-functions/checkpoint-004-square-root.playground/Contents.swift
@@ -1,3 +1,35 @@
 import Cocoa
 
+enum SquareRootError: Error {
+  // Proper capitalization in outOfBounds
+  case outOfBounds, noSquareRoot
+}
 
+// Added return type of Int to return the square root
+func getSquareRoot(Of number: Int) throws -> Int {
+  // guard instead of if for early returns
+  guard number >= 1 && number <= 10_000 else {
+    throw SquareRootError.outOfBounds
+  }
+  
+  // Cleaner way of getting the first number that matches
+  let result = (1...100)
+    .first { $0 * $0 == number }
+  
+  // guard again
+  guard let result = result else {
+    throw SquareRootError.noSquareRoot
+  }
+  
+  // Return the result
+  return result
+}
+
+do {
+  let squareRoot = try getSquareRoot(Of: 9)
+  print(squareRoot)
+} catch SquareRootError.outOfBounds {
+  print("Please use a number between 1 and 10,000")
+} catch SquareRootError.noSquareRoot {
+  print("There is no even square root to this problem")
+}
