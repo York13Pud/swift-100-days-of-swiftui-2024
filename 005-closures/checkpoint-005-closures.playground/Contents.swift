5ff51189fa551224f4ed647b2e182d36933b5582
dev_neil_a <neilallwood@icloud.com>
2024-01-11 15:56:49 +0000

    Completed checkpoint 5

--- 005-closures/checkpoint-005-closures.playground/Contents.swift
+++ 005-closures/checkpoint-005-closures.playground/Contents.swift
@@ -1,3 +1,16 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// Define the array of numbers:
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+// Use a closure to perform:
+let processLuckyNumbers = { (source: [Int]) in
+    // Filter out the even numbers:
+    source.filter { !$0.isMultiple(of: 2) }
+          .sorted() // Sort the remaining odd numbers assentingly
+          .map() // Map the numbers to a new array
+    { print ("\($0) is a lucky number")} // Print out each row
+}
+
+// Process the array of numbers:
+processLuckyNumbers(luckyNumbers)
