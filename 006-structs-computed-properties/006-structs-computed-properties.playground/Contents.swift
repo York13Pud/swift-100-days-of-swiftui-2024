a35078e62279da6f19a1527041d87769c51c42ae
dev_neil_a <neilallwood@icloud.com>
2024-01-12 15:09:27 +0000

    Added additional examples

--- 006-structs-computed-properties/006-structs-computed-properties.playground/Contents.swift
+++ 006-structs-computed-properties/006-structs-computed-properties.playground/Contents.swift
@@ -41,3 +41,71 @@ struct Employee {
 var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
 archer.takeVacation(days: 5)
 print(archer.vacationRemaining)
+
+// Compute property values dynamically
+
+// Simple computed property:
+struct EmployeeCP {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+
+    var vacationRemaining: Int {
+        vacationAllocated - vacationTaken
+    }
+}
+
+var archerTwo = EmployeeCP(name: "Sterling Archer", vacationAllocated: 14)
+
+archerTwo.vacationTaken += 4
+print(archerTwo.vacationRemaining)
+archerTwo.vacationTaken += 4
+print(archerTwo.vacationRemaining)
+
+// Using get and set:
+struct EmployeeCPTwo {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+        
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var archerThree = EmployeeCPTwo(name: "Sterling Archer", vacationAllocated: 20)
+
+archerThree.vacationTaken += 5
+print(archerThree.vacationAllocated)
+print(archerThree.vacationTaken)
+
+archerThree.vacationRemaining = 5
+print(archerThree.vacationRemaining)
+print(archerThree.vacationAllocated)
+
+// Property Change Actions
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
