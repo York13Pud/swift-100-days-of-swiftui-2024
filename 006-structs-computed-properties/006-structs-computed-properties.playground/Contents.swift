8d8b825ffd1f3accda799f6d3c7121141a5c5a6d
dev_neil_a <neilallwood@icloud.com>
2024-01-12 12:09:45 +0000

    Added examples of structs

--- 006-structs-computed-properties/006-structs-computed-properties.playground/Contents.swift
+++ 006-structs-computed-properties/006-structs-computed-properties.playground/Contents.swift
@@ -1,3 +1,43 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// Structs
+
+// A simple struct example:
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+// Another example. This time with a mutating method to update a variable:
+struct Employee {
+    let name: String
+    var vacationRemaining: Int = 25
+
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
