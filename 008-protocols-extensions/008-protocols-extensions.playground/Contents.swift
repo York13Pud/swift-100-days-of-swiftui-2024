48ef296038ae8717c393c1f92736780a41afa23b
dev_neil_a <neilallwood@icloud.com>
2024-01-16 13:41:55 +0000

    Added examples

--- 008-protocols-extensions/008-protocols-extensions.playground/Contents.swift
+++ 008-protocols-extensions/008-protocols-extensions.playground/Contents.swift
@@ -1,2 +1,31 @@
 import Cocoa
 
+protocol Vehicle {
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+struct Car: Vehicle {
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km.")
+    }
+
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+}
+
+func commute(distance: Int, using vehicle: Car) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
