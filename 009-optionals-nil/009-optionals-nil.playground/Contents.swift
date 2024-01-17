e975d91d59c14dbf0c2b3c54d1ce33d0fbd27648
dev_neil_a <neilallwood@icloud.com>
2024-01-17 11:47:13 +0000

    Added examples

--- 009-optionals-nil/009-optionals-nil.playground/Contents.swift
+++ 009-optionals-nil/009-optionals-nil.playground/Contents.swift
@@ -1,2 +1,17 @@
 import Cocoa
 
+var username: String? = nil
+
+func checkUsername(_ toBeChecked: String?) {
+    if let unwrappedName = toBeChecked {
+        print("We got a user: \(unwrappedName)")
+    } else {
+        print("The optional was empty.")
+    }
+}
+
+checkUsername(username) // Prints "The optional was empty."
+
+username = "Fred"
+
+checkUsername(username) // Prints "We got a user: Fred"
