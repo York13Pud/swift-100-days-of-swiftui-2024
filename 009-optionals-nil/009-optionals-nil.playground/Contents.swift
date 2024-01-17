ed883ae05446ff88d22a088fe6ca8c39cb9c03ac
dev_neil_a <neilallwood@icloud.com>
2024-01-17 14:25:32 +0000

    Added examples

--- 009-optionals-nil/009-optionals-nil.playground/Contents.swift
+++ 009-optionals-nil/009-optionals-nil.playground/Contents.swift
@@ -15,3 +15,20 @@ checkUsername(username) // Prints "The optional was empty."
 username = "Fred"
 
 checkUsername(username) // Prints "We got a user: Fred"
+
+// Unwrap Optionals With Guard
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        // Run if number is nil:
+        print("Missing input")
+
+        // 1: We *must* exit the function here
+        return
+    }
+    // Run if number has a value
+    // 2: `number` is still available outside of `guard`
+    print("\(number) x \(number) is \(number * number)")
+}
+
+printSquare(of: 5)
