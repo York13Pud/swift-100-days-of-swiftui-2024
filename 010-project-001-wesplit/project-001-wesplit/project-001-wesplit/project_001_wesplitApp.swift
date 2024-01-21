d57416adc3d47a7e54a6b634abe5ab85001eb1ce
dev_neil_a <neilallwood@icloud.com>
2024-01-21 15:20:00 +0000

    Added file for wesplit project

---
+++ 010-project-001-wesplit/project-001-wesplit/project-001-wesplit/project_001_wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_001_wesplitApp.swift
+//  project-001-wesplit
+//
+//  Created by Neil Allwood on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct project_001_wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
