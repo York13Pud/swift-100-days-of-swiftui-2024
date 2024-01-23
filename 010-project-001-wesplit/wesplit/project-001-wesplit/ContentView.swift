6e4fb38a14a6821f39107035a0ecc5abd31cd51c
dev_neil_a <neilallwood@icloud.com>
2024-01-23 16:20:41 +0000

    Updated code

--- 010-project-001-wesplit/wesplit/project-001-wesplit/ContentView.swift
+++ 010-project-001-wesplit/wesplit/project-001-wesplit/ContentView.swift
@@ -11,23 +11,76 @@ struct ContentView: View {
     // Define the required variables and constants:
     @State private var checkAmount: Double = 0.0
     @State private var numberOfPeople: Int = 2
-    @State private var tipPercentage: Int = 2
+    @State private var tipPercentage: Int = 10
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Form {
-            Section {
-                TextField("Aount", 
-                          value: $checkAmount,
-                          format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                )
-                .keyboardType(.decimalPad) // Sets the keyboard type to a number pad on appear
-            }
-            
-            Section {
-                Text(checkAmount,
-                     format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                )
+        NavigationStack {
+            ZStack {
+                Color.gray
+                     .ignoresSafeArea()
+                
+                VStack {
+                    
+                    Divider()
+                        .background(Color.gray)
+                    
+                    Form {
+                        
+                        Section {
+                            TextField("Aount",
+                                      value: $checkAmount,
+                                      format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                            )
+                            .keyboardType(.decimalPad) // Sets the keyboard type to a number pad on appear
+                            
+                            Picker("Number Of People", selection: $numberOfPeople) {
+                                ForEach(2..<100, id: \.self) {
+                                    Text("\($0) People")
+                                }
+                            }
+                            .pickerStyle(.navigationLink)
+                        }
+                        
+                        Section {
+                            Picker("Tip Percentage", selection: $tipPercentage) {
+                                ForEach(tipPercentages, id: \.self) {
+                                    Text($0, format: .percent)
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                        
+                        Section {
+                            Text(totalPerPerson,
+                                 format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                            )
+                        }
+                    }
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            VStack {
+                                Text("WeSplit")
+                                    .font(.title)
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                    .foregroundStyle(Color.white)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
