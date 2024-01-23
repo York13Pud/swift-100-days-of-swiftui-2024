41a4364e0ec977c6dc1bb73cf61f28ffebc59f87
dev_neil_a <neilallwood@icloud.com>
2024-01-23 12:02:46 +0000

    Updated code

--- 010-project-001-wesplit/wesplit/project-001-wesplit/ContentView.swift
+++ 010-project-001-wesplit/wesplit/project-001-wesplit/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Define the required variables and constants:
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Aount", 
+                          value: $checkAmount,
+                          format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                )
+                .keyboardType(.decimalPad) // Sets the keyboard type to a number pad on appear
+            }
+            
+            Section {
+                Text(checkAmount,
+                     format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                )
+            }
         }
-        .padding()
     }
 }
 
