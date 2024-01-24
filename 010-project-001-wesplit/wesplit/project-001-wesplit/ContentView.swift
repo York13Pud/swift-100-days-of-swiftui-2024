4e7976fa513d339c249f8ab3af68afe74692b5e8
dev_neil_a <neilallwood@icloud.com>
2024-01-24 12:36:58 +0000

    Completed Project 1 - WeSplit

--- 010-project-001-wesplit/wesplit/project-001-wesplit/ContentView.swift
+++ 010-project-001-wesplit/wesplit/project-001-wesplit/ContentView.swift
@@ -11,86 +11,109 @@ struct ContentView: View {
     // Define the required variables and constants:
     @State private var checkAmount: Double = 0.0
     @State private var numberOfPeople: Int = 2
-    @State private var tipPercentage: Int = 10
+    @State private var tipPercentage: Double = 10
     
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
+    // let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
+    var totalPerPerson: Array<Double> {
         let peopleCount = Double(numberOfPeople)
         let tipSelection = Double(tipPercentage)
 
-        let tipValue = checkAmount / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
+        let tipValue: Double = checkAmount / 100 * tipSelection
+        let grandTotal: Double = checkAmount + tipValue
+        let amountPerPerson: Double = grandTotal / peopleCount
 
-        return amountPerPerson
+        return [tipValue, grandTotal, amountPerPerson]
     }
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                Color.gray
-                     .ignoresSafeArea()
+            
+            Form {
                 
-                VStack {
-                    
-                    Divider()
-                        .background(Color.gray)
-                    
-                    Form {
-                        
-                        Section {
-                            TextField("Aount",
-                                      value: $checkAmount,
-                                      format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                            )
-                            .keyboardType(.decimalPad) // Sets the keyboard type to a number pad on appear
-                            .focused($amountIsFocused)
-                            
-                            Picker("Number Of People", selection: $numberOfPeople) {
-                                ForEach(2..<100, id: \.self) {
-                                    Text("\($0) People")
-                                }
+                Section("Bill Amount") {
+                    HStack {
+                        Text("Enter Bill Amount")
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                        TextField("Amount",
+                                  value: $checkAmount,
+                                  format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                        )
+                        .keyboardType(.decimalPad) // Sets the keyboard type to a number pad on appear
+                        .focused($amountIsFocused)
+                        .multilineTextAlignment(.trailing)
+                    }
+                }
+                
+                Section("Split The Bill Between") {
+                    Picker("Number Of People", selection: $numberOfPeople) {
+                        ForEach(1..<100, id: \.self) {
+                            if $0 == 1 {
+                                Text("\($0) Person")
+                            } else {
+                                Text("\($0) People")
                             }
-                            .pickerStyle(.navigationLink)
-                        }
-                        
-                        Section {
-                            Picker("Tip Percentage", selection: $tipPercentage) {
-                                ForEach(tipPercentages, id: \.self) {
-                                    Text($0, format: .percent)
-                                }
-                            }
-                            .pickerStyle(.segmented)
-                        }
-                        
-                        Section {
-                            Text(totalPerPerson,
-                                 format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                            )
                         }
                     }
-                    .navigationBarTitleDisplayMode(.automatic)
-                    .navigationTitle("WeSplit")
-                    .toolbar {
-                        // Add button to hide the keyboard. Shows up in the top right.
-                        if amountIsFocused {
-                            Button("Done") {
-                                amountIsFocused = false
-                            }.foregroundColor(.white)
-                        }
-                        
-//                        ToolbarItem(placement: .principal) {
-//                            VStack {
-//                                Text("WeSplit")
-//                                    .font(.title)
-//                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-//                                    .foregroundStyle(Color.white)
-//                            }
-//                        }
-
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Select The Tip Amount") {
+                    // Picker("Tip Percentage", selection: $tipPercentage) {
+                    //    ForEach(tipPercentages, id: \.self) {
+                    //        Text($0, format: .percent)
+                    //    }
+                    // }
+                    // .pickerStyle(.segmented)
+                    
+                    Slider(value: $tipPercentage, in: 0...100, step: 5)
+                    
+                    Text("\(Int(tipPercentage))%")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+               
+                Section("Total") {
+                    
+                    HStack {
+                        Text("Tip Amount")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(totalPerPerson[0],
+                             format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                        ).frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    
+                    HStack {
+                        Text("Bill Amount")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(totalPerPerson[1],
+                             format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                        ).frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                }
+                
+                Section() {
+                
+                    HStack {
+                        Text("Each Person Pays")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(totalPerPerson[2],
+                             format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                        )
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    }.foregroundColor(.accentColor)
+    
+                }
+            }
+            .navigationBarTitleDisplayMode(.automatic)
+            .navigationTitle("WeSplit")
+            .toolbar {
+                // Add button to hide the keyboard. Shows up in the top right.
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
             }
