08de35ceb48df24c6cad55294c390da6db7c44d4
dev_neil_a <neilallwood@icloud.com>
2024-01-25 15:36:51 +0000

    Started challenge 1 project

---
+++ 011-challenge-01-temp-converter/temp-converter/temp-converter/temp_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  temp_converterApp.swift
+//  temp-converter
+//
+//  Created by Neil Allwood on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct temp_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
