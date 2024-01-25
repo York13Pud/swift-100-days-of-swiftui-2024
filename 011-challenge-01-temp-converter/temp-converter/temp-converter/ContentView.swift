fce8e74cf95208c91d4a0e309b8d6e0b910520b5
dev_neil_a <neilallwood@icloud.com>
2024-01-25 17:17:39 +0000

    Completed challenge 1 - temp converter

--- 011-challenge-01-temp-converter/temp-converter/temp-converter/ContentView.swift
+++ 011-challenge-01-temp-converter/temp-converter/temp-converter/ContentView.swift
@@ -8,15 +8,120 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    // Define required vars and lets:
+    @State private var temp: Double = 0.0
+    @State private var tempFrom: String = "Celsius"
+    @State private var tempTo: String = "Fahrenheit"
+    
+    @FocusState private var isFocused: Bool
+    
+    private let tempUnits: Array<String> = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    private var tempResult: Double {
+            
+        if tempFrom == "Celsius" && tempTo == "Fahrenheit" {
+            let convertedValue = (temp * 9/5) + 32
+            return convertedValue
+            
+        } else if tempFrom == "Celsius" && tempTo == "Kelvin" {
+            let convertedValue = temp + 273.15
+            return convertedValue
+            
+        } else if tempFrom == "Fahrenheit" && tempTo == "Celsius" {
+            let convertedValue = (temp - 32) * 5/9
+            return convertedValue
+            
+        } else if tempFrom == "Fahrenheit" && tempTo == "Kelvin" {
+            let convertedValue = (temp - 32) * 5/9 + 273.15
+            return convertedValue
+            
+        } else if tempFrom == "Kelvin" && tempTo == "Celsius" {
+            let convertedValue = temp - 273.15
+            return convertedValue
+            
+        } else if tempFrom == "Kelvin" && tempTo == "Fahrenheit" {
+            let convertedValue = (temp - 273.15) * 9/5 + 32
+            return convertedValue
+            
+        } else {
+            return 0.0
+            
         }
-        .padding()
+        
     }
+    
+    var body: some View {
+        NavigationStack {
+            
+            Form {
+                
+                Section("Convert") {
+                    HStack {
+                        Text("Temperature (°\(tempFrom.first?.description ?? ""))".capitalized)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                        TextField("Temperature)",
+                                  value: $temp,
+                                  format: .number)
+                        .multilineTextAlignment(.trailing)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    }
+                }
+                
+                Section("From") {
+                    Picker("From", selection: $tempFrom) {
+                        ForEach(tempUnits, id: \.self) {
+                            if $0 != tempTo {
+                                Text($0)
+                            }
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("To"){
+                    Picker("To", selection: $tempTo) {
+                        ForEach(tempUnits, id: \.self) {
+                            if $0 != tempFrom {
+                                Text($0)
+                            }
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Converting") {
+                    HStack {
+                        Text("\(tempFrom) To \(tempTo) is".capitalized)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                            .foregroundStyle(Color.accentColor)
+                        
+                        Text("\(tempResult, specifier: "%.2f")°\(tempTo.first?.description ?? "")")
+                            .multilineTextAlignment(.trailing)
+                            .foregroundStyle(Color.accentColor)
+                    }
+                }
+                
+            }
+            .navigationTitle("Temp Converter")
+            .navigationBarTitleDisplayMode(.automatic)
+            
+            // Show a Done button on the decimal keyboard in the top right:
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
 }
 
 #Preview {
