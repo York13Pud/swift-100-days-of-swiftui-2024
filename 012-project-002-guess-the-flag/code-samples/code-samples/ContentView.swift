53d51834518303770365156cb9fbac35ff276856
dev_neil_a <neilallwood@icloud.com>
2024-01-26 13:03:08 +0000

    Added project 2 code samples

---
+++ 012-project-002-guess-the-flag/code-samples/code-samples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  code-samples
+//
+//  Created by Neil Allwood on 26/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
