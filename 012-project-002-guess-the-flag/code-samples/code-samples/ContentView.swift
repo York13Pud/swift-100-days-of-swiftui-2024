35f9e52118ebf3fa7b074afae2b8751b22089945
dev_neil_a <neilallwood@icloud.com>
2024-01-26 15:02:06 +0000

    Added some colour examples

--- 012-project-002-guess-the-flag/code-samples/code-samples/ContentView.swift
+++ 012-project-002-guess-the-flag/code-samples/code-samples/ContentView.swift
@@ -2,20 +2,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20){
-            Text("This is a")
-            Text("VStack")
-        }
-        .padding()
-
-        VStack {
+        ZStack {
             
-            HStack {
-                Text("This is a")
-                Text("HStack inside a VStack")
+            Color.orange
+            
+            VStack {
+                
+                VStack() {
+                    Text("This is a")
+                    Text("VStack")
+                }
+                .padding(50)
+                .background(.ultraThinMaterial) // This adds a box behind the VStack that
+                                                // gives a frosted glass effect.
+                
+                VStack {
+                    // A 3 x 3 grid example using a VStack and HStacks
+                    HStack(spacing: 30) {
+                        Text("1")
+                        Text("2")
+                        Text("3")
+                    }
+                    .frame(minWidth: 150, maxWidth: .infinity, maxHeight: 30)
+                    .background(.red)
+                    .foregroundColor(.white)
+                    
+                    HStack(spacing: 30) {
+                        Text("4")
+                        Text("5")
+                        Text("6")
+                    }
+                    .frame(minWidth: 150, maxWidth: .infinity, maxHeight: 30)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    
+                    HStack(spacing: 30) {
+                        Text("7")
+                        Text("8")
+                        Text("9")
+                    }
+                    .frame(minWidth: 150, maxWidth: .infinity, maxHeight: 30)
+                    .background(.green)
+                    .foregroundColor(.white)
+                }
+                .padding()
             }
         }
-        .padding()
+        .ignoresSafeArea() // Allows the background colour to go the full height of the                        screen
     }
 }
 
