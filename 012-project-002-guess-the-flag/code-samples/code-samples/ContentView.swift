593f77c61f696900f5f61f9d8d5558a64f9910a2
dev_neil_a <neilallwood@icloud.com>
2024-01-26 17:06:56 +0000

    Added button examples

--- 012-project-002-guess-the-flag/code-samples/code-samples/ContentView.swift
+++ 012-project-002-guess-the-flag/code-samples/code-samples/ContentView.swift
@@ -1,10 +1,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
     var body: some View {
+        
         ZStack {
-            
-            Color.orange
+            // Use a linear gradient for the background
+            LinearGradient(colors: [.orange, .yellow],
+                           startPoint: .top,
+                           endPoint: .bottom)
             
             VStack {
                 
@@ -14,7 +20,7 @@ struct ContentView: View {
                 }
                 .padding(50)
                 .background(.ultraThinMaterial) // This adds a box behind the VStack that
-                                                // gives a frosted glass effect.
+                // gives a frosted glass effect.
                 
                 VStack {
                     // A 3 x 3 grid example using a VStack and HStacks
@@ -24,7 +30,7 @@ struct ContentView: View {
                         Text("3")
                     }
                     .frame(minWidth: 150, maxWidth: .infinity, maxHeight: 30)
-                    .background(.red)
+                    .background(.red.gradient)
                     .foregroundColor(.white)
                     
                     HStack(spacing: 30) {
@@ -33,7 +39,7 @@ struct ContentView: View {
                         Text("6")
                     }
                     .frame(minWidth: 150, maxWidth: .infinity, maxHeight: 30)
-                    .background(.blue)
+                    .background(.blue.gradient)
                     .foregroundColor(.white)
                     
                     HStack(spacing: 30) {
@@ -42,14 +48,75 @@ struct ContentView: View {
                         Text("9")
                     }
                     .frame(minWidth: 150, maxWidth: .infinity, maxHeight: 30)
-                    .background(.green)
+                    .background(.green.gradient)
                     .foregroundColor(.white)
                 }
                 .padding()
+                
+                VStack {
+                    
+                    HStack {
+                        Button("Nothing", role: .none) {}
+                            .buttonStyle(.borderedProminent)
+                            .tint(.green)
+                        
+                        Button("Something", role: .cancel) {}
+                            .buttonStyle(.borderedProminent)
+                        
+                        // Using action means calling a function. No trailing closure required
+                        Button("Delete", role: .destructive, action: executeDelete)
+                            .buttonStyle(.borderedProminent)
+                    }
+                    HStack(spacing: 38) {
+                        
+                        // A fully custom button with an image in the button
+                        Button {
+                            print("I was edited")
+                        } label: {
+                            Label("Edit", systemImage: "pencil")
+                                .padding()
+                                .foregroundStyle(.white)
+                                .background(.indigo)
+                                .font(.title3)
+                        }
+                        
+                        Button {
+                            print("Alert")
+                            showingAlert = true
+                        } label: {
+                            Label("Message", systemImage: "mail")
+                                .padding()
+                                .foregroundStyle(.white)
+                                .background(.teal)
+                                .font(.title3)
+                        }
+                    }
+
+                } 
+                .alert("Warning", isPresented: $showingAlert) {
+                    Button("Ok", role: .destructive) {
+                        print("Cleared")
+                    }
+                    
+                    Button("Cancel", role: .cancel) {
+                        print("Cancelled")
+                    }
+                    
+                } message: {
+                    Text("This message will self-destruct in five seconds!")
+                }
+                
             }
+            
         }
-        .ignoresSafeArea() // Allows the background colour to go the full height of the                        screen
+        .ignoresSafeArea() // Allows the background colour to go the full height of the screen
+        
     }
+    
+    func executeDelete() {
+        print("Deleting.....")
+    }
+    
 }
 
 #Preview {
