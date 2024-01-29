c5a56d55876fff0c6483485133ce443bd33ef1d2
dev_neil_a <neilallwood@icloud.com>
2024-01-29 08:11:36 +0000

    Added basic layout to app

--- 012-project-002-guess-the-flag/guess-the-flag/guess-the-flag/ContentView.swift
+++ 012-project-002-guess-the-flag/guess-the-flag/guess-the-flag/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            
+            Color.gray
+            
+            VStack(spacing: 30) {
+                
+                VStack {
+                    Text("Tap the flag of".capitalized)
+                    Text(countries[correctAnswer])
+                }
+                .foregroundStyle(.white)
+                .padding()
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // Code for button when tapped
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
+        } .ignoresSafeArea()
     }
 }
 
