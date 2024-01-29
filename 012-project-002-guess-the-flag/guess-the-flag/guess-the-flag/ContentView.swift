870fe808d97155d8925982fc8aabf831a772c5a7
dev_neil_a <neilallwood@icloud.com>
2024-01-29 12:14:23 +0000

    Completed project 2 - guess the flag

--- 012-project-002-guess-the-flag/guess-the-flag/guess-the-flag/ContentView.swift
+++ 012-project-002-guess-the-flag/guess-the-flag/guess-the-flag/ContentView.swift
@@ -13,15 +13,21 @@ struct ContentView: View {
     @State private var correctAnswer: Int = Int.random(in: 0...2)
     
     @State private var playerScore: Int = 0
+    @State private var roundsPlayed: Int = 0
+    let roundsToPlay = 8
     
     // Vars for the score alert:
     @State private var showingScore: Bool = false
+    @State private var gameOver: Bool = false
+    
     @State private var scoreTitle: String = ""
+    @State private var scoreMessage: String = ""
     
     var body: some View {
         ZStack {
             // Set background colour:
-            //LinearGradient(colors: [.purple, .mint], startPoint: .top, endPoint: .bottom)
+            // LinearGradient(colors: [.purple, .mint], startPoint: .top, endPoint: .bottom)
+            
             RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
                            center: .top, startRadius: 200, endRadius: 700)
@@ -68,39 +74,76 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
 
-                Text("Score: ZZZ")
+                Text("Your Score: \(playerScore)")
                     .foregroundStyle(.white)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.weight(.bold))
+                
+                Text("Round: \(roundsPlayed) of \(roundsToPlay)")
+                    .foregroundStyle(.white)
+                    .font(.headline.weight(.bold))
 
                 Spacer()
+                
             }
-            
-            
+            // Alert that shows the users score:
             .alert(scoreTitle, isPresented: $showingScore) {
                 Button("Continue", action: askQuestion)
             } message: {
-                Text("Your Score Is: XXXXX")
+                Text(scoreMessage)
                     .foregroundStyle(.white)
             }
+            
+            // Alert that shows the game over message and score
+            .alert(scoreTitle, isPresented: $gameOver) {
+                Button("New Game", action: resetGame)
+            } message: {
+                Text(scoreMessage)
+                    .foregroundStyle(.white)
+            }
+            
             .padding()
             
         }
-
         
     }
+    
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
+            playerScore += 1
             scoreTitle = "Correct"
+            scoreMessage = "Your Score Is: \(playerScore)"
+            
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Incorrect"
+            scoreMessage = "That is the flag of \(countries[number])"
         }
         
+        roundsPlayed += 1
         showingScore = true
     }
     
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        if roundsPlayed == roundsToPlay {
+            scoreMessage = "Game Over"
+            scoreMessage = "After \(roundsToPlay) rounds, your final score is \(playerScore)"
+            gameOver = true
+        } else {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
+    }
+    
+    func resetGame() {
+        // Reset vars to defaults:
+        playerScore = 0
+        roundsPlayed = 0
+        showingScore = false
+        gameOver = false
+        scoreTitle = ""
+        scoreMessage = ""
+        
+        // Call askQuestion function:
+        askQuestion()
     }
 }
 
