f6c41d4ed08b9111c6af352287accbe7fa9a3383
dev_neil_a <neilallwood@icloud.com>
2024-01-29 11:31:05 +0000

    Updated layout

--- 012-project-002-guess-the-flag/guess-the-flag/guess-the-flag/ContentView.swift
+++ 012-project-002-guess-the-flag/guess-the-flag/guess-the-flag/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     
     @State private var correctAnswer: Int = Int.random(in: 0...2)
     
+    @State private var playerScore: Int = 0
+    
     // Vars for the score alert:
     @State private var showingScore: Bool = false
     @State private var scoreTitle: String = ""
@@ -19,39 +21,71 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Set background colour:
-            LinearGradient(colors: [.purple, .mint], startPoint: .top, endPoint: .bottom)
+            //LinearGradient(colors: [.purple, .mint], startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                                   .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+                           center: .top, startRadius: 200, endRadius: 700)
+                           .ignoresSafeArea()
 
-            VStack(spacing: 30) {
+            VStack {
+                Spacer()
                 
-                VStack {
-                    Text("Tap the flag of:".capitalized)
-                        .font(.subheadline.weight(.heavy))
+                Text("Guess The Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                
+                Spacer()
+                Spacer()
+                
+                VStack(spacing: 15) {
                     
-                    Text(countries[correctAnswer])
-                        .font(.largeTitle.weight(.semibold))
-                }
-                .foregroundStyle(.white)
-                .padding()
-                
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        // Image will get the image from the asset library that matches the name provided below:
-                        Image(countries[number])
-                            .clipShape(.capsule)
-                            .shadow(radius: 5)
+                    VStack {
+                        Text("Tap the flag of:".capitalized)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    .foregroundStyle(.secondary)
+                    .padding()
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            // Image will get the image from the asset library that matches the name provided below:
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 12)
+                        }
                     }
                 }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+
+                Text("Score: ZZZ")
+                    .foregroundStyle(.white)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.weight(.bold))
+
+                Spacer()
             }
-        } 
-        .ignoresSafeArea()
-        
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your Score Is: XXXXX")
+            
+            
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your Score Is: XXXXX")
+                    .foregroundStyle(.white)
+            }
+            .padding()
+            
         }
+
         
     }
     func flagTapped(_ number: Int) {
