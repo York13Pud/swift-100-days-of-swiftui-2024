01b057807cf3d9f9a865c50a535d79e61315db42
dev_neil_a <neilallwood@icloud.com>
2024-01-29 07:53:50 +0000

    Added files for guess the flag project

---
+++ 012-project-002-guess-the-flag/guess-the-flag/guess-the-flag/guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagApp.swift
+//  guess-the-flag
+//
+//  Created by Neil Allwood on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
