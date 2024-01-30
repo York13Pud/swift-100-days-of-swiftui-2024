b45dfcc92bb0806abbe65dfa323dd788926a87d2
dev_neil_a <neilallwood@icloud.com>
2024-01-30 14:12:27 +0000

    Added new project for views and modifiers

---
+++ 013-project-003-views-modifiers/views-and-modifiers/views-and-modifiers/views_and_modifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  views_and_modifiersApp.swift
+//  views-and-modifiers
+//
+//  Created by Neil Allwood on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct views_and_modifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
