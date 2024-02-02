13585f89cd224771227e84a0e9b5010c4a68e752
dev_neil_a <neilallwood@icloud.com>
2024-02-02 14:13:47 +0000

    Updated and removed code that wasn't suitable

--- 015-challenge-003-rock-paper-scissors/rock-paper-scissors/rock-paper-scissors/ContentView.swift
+++ 015-challenge-003-rock-paper-scissors/rock-paper-scissors/rock-paper-scissors/ContentView.swift
@@ -23,37 +23,15 @@ extension View {
     }
 }
 
-// A View composition for each player option button:
-struct PlayerChoiceButton: View {
-    let symbol: String
-    let text: String
-    
-    var body: some View {
-        Button {
-            print(" ")
-        } label: {
-            VStack {
-                Text("\(symbol)")
-                    .font(.system(size: 70))
-                Text("\(text.capitalized)")
-                    .font(.title)
-            }
-        }
-        .buttonStyle()
-        
-        Spacer()
-    }
-}
-
 import SwiftUI
 
 struct ContentView: View {
     
-    private var moves = [
-        "paper": "✋",
+    var moves = [
         "rock": "✊",
+        "paper": "✋",
         "scissors": "✌️"
-    ]
+    ].sorted(by: {$0.value < $1.value}) // Converted to an array of tuples and maintains the order
     
     @State private var appScore: Int = 0
     @State private var playerScore: Int = 0
@@ -61,7 +39,10 @@ struct ContentView: View {
     @State private var appChoice: String = ""
     @State private var playerChoice: String = ""
     
+    @State private var roundsPlayed: Int = 1
+    
     var body: some View {
+
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.purple, .cyan]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
@@ -71,14 +52,40 @@ struct ContentView: View {
                 HStack {
                     
                     Spacer()
-                    
-                    ForEach(moves.sorted(by: <), id: \.key) { key, value in
-                        PlayerChoiceButton(symbol: value, text: key)
+                    ForEach(moves, id: \.key) { key, value in
+                        Button {
+                            buttonPressed()
+                        } label: {
+                            VStack {
+                                Text("\(value)")
+                                    .font(.system(size: 70))
+                                Text("\(key.capitalized)")
+                                    .font(.title)
+                            }
+                        }
+                        .buttonStyle()
+                        
+                        Spacer()
                     }
                 }
             }
         }
     }
+    
+    // Program logic goes here:
+    func buttonPressed() {
+        appChoice = moves[Int.random(in: 0...2)].key
+
+    }
+    
+    func nextRoundOrGameOver() {
+        
+    }
+    
+    func restartGame() {
+        
+    }
+    
 }
 
 #Preview {
