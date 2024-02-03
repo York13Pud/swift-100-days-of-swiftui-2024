eb0c2106de8eeb538d47445f38e7d996ce064d8f
dev_neil_a <neilallwood@icloud.com>
2024-02-03 10:52:19 +0000

    Added new code samples project for project 4

---
+++ 016-project-004-better-rest/code-examples/code-examples/code_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_examplesApp.swift
+//  code-examples
+//
+//  Created by Neil Allwood on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct code_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
