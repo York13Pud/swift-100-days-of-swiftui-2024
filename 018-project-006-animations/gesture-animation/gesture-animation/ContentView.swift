dd4336fcd4140d8890a71c1b1289e03e1132c127
dev_neil_a <neilallwood@icloud.com>
2024-02-07 18:32:26 +0000

    Added layout and basic logic

--- 018-project-006-animations/gesture-animation/gesture-animation/ContentView.swift
+++ 018-project-006-animations/gesture-animation/gesture-animation/ContentView.swift
@@ -3,14 +3,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero // CGSise = core graphics (older framework).
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+                    // The delay staggers the letters when moving, similar to a mouse trail.
+            }
         }
-        .padding()
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
---
+++ 019-challenge-004-times-tables/multiplication-tables/multiplication-tables/multiplication_tablesApp.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct multiplication_tablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
