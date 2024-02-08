18d5f8e4605c30638daefbc0f34e3f546ecb2de2
dev_neil_a <neilallwood@icloud.com>
2024-02-08 15:56:37 +0000

    Completed challenge 4 - Times table app

--- 019-challenge-004-times-tables/multiplication-tables/multiplication-tables/ContentView.swift
+++ 019-challenge-004-times-tables/multiplication-tables/multiplication-tables/ContentView.swift
@@ -12,138 +12,231 @@ struct ContentView: View {
     @State private var playerAnswer: Int = 0
     @State private var playerScore: Int = 0
     
+    @State private var answerCorrectShow: Bool = false
+    @State private var answerCorrect: Bool = false
+
+    @State private var answerButtonDisabled: Bool = false
+    
     @State private var isNewGame: Bool = false
+    @State private var isGameOver: Bool = false
+    
     @State private var scale: Double = 1.0
+    
     @FocusState private var answerIsFocused: Bool
     
     var body: some View {
         NavigationStack {
 
-            // 1. Add a method to select which times table to use, between 2 and 12.
-            //    Buttons, slider or drop-down.
             Form {
-                
-                List {
-                    // Main menu
-                    if !isNewGame {
-                        Group {
-                            
-                            Section("Times Table") {
-                                Picker("Select Times Table", selection: $timesTableSelected) {
-                                    ForEach(2..<13, id: \.self) {
-                                        Text("\($0) Times Table")
-                                    }
+                // Main menu
+                if !isNewGame {
+                    Group {
+                        
+                        Section("Times Table") {
+                            Picker("Select Times Table", selection: $timesTableSelected) {
+                                ForEach(2..<13, id: \.self) {
+                                    Text("\($0) Times Table")
                                 }
-                                .pickerStyle(.navigationLink)
                             }
+                            .pickerStyle(.navigationLink)
+                        }
+                        
+                        Section("Questions") {
+                            // 2. Add a method to select the number of questions to show
+                            Slider(value: $questionsToAsk, in: 5...20, step: 5)
                             
-                            Section("Questions") {
-                                // 2. Add a method to select the number of questions to show
-                                Slider(value: $questionsToAsk, in: 5...20, step: 5)
+                            Text("Ask \(Int(questionsToAsk)) Questions")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                        
+                        Section() {
+                            
+                            Button() {
+                                questionsAsked = 1
                                 
-                                Text("Ask \(Int(questionsToAsk)) Questions")
-                                    .frame(maxWidth: .infinity, alignment: .center)
+                                playerAnswer = 0
+                                playerScore = 0
+                                
+                                answerCorrectShow = false
+                                answerCorrect = false
+
+                                answerButtonDisabled = false
+                                
+                                isNewGame = false
+                                isGameOver = false
+                                
+                                withAnimation {
+                                    isNewGame.toggle()
+                                }
+                                
+                            } label: {
+                                Text("Start Game")
+                                    .frame(maxWidth: .infinity)
+                                    .foregroundStyle(.white)
                             }
-                            
-                            Section() {
+                            .listRowBackground(Color.clear)
+                            .buttonStyle(.borderedProminent)
+                        }
+                    }
+                }
+                
+                // Main game section
+                else {
+                    Group {
+                        
+                        Section("Question \(questionsAsked) of \(Int(questionsToAsk))") {
+                            HStack {
+                                Text("What is \(timesTableSelected) x \(timesTableMultiplier)")
+                                TextField("", value: $playerAnswer, format: .number)
+                                    .keyboardType(.decimalPad) // Sets the keyboard type to a number pad on appear
+                                    .focused($answerIsFocused)
+                                    .multilineTextAlignment(.trailing)
+                            }
+                        }
+                    
+                        Section {
+                            HStack {
+                                Text("Your Score Is: \(playerScore)")
+                            }
+                        }
+                        
+                        Section {
+                            HStack {
                                 Button() {
                                     withAnimation {
                                         isNewGame.toggle()
                                     }
                                 } label: {
-                                    Text("Start Game")
+                                    Text("New Game")
                                         .frame(maxWidth: .infinity)
                                         .foregroundStyle(.white)
                                 }
                                 .listRowBackground(Color.clear)
+                                .tint(.red)
                                 .buttonStyle(.borderedProminent)
+                                
+                                
+                                Spacer()
+                                Spacer()
+                                
+                                Button() {
+                                    print("Button 1a: \(answerButtonDisabled)")
+                                    answerButtonDisabled.toggle()
+                                    checkAnswer()
+                                    print("Button 1b: \(answerButtonDisabled)")
+                                    
+                                } label: {
+                                    Text("Check Answer")
+                                        .frame(maxWidth: .infinity)
+                                        .foregroundStyle(.white)
+                                    
+                                }
+                                .listRowBackground(Color.clear)
+                                .tint(.green)
+                                .buttonStyle(.borderedProminent)
+                                .disabled(answerButtonDisabled ? true : false)
                             }
                         }
+                        .listRowBackground(Color.clear)
                     }
                     
-                    // Main game section
-                    else {
-                        Group {
-                            
-                            Section("Question \(questionsAsked) of \(Int(questionsToAsk))") {
-                                HStack {
-                                    Text("What is \(timesTableSelected) x \(timesTableMultiplier)")
-                                    TextField("", value: $playerAnswer, format: .number)
-                                        .keyboardType(.decimalPad) // Sets the keyboard type to a number pad on appear
-                                        .focused($answerIsFocused)
-                                        .multilineTextAlignment(.trailing)
-                                }
-                            }
-                            
-                            Section {
-                                HStack {
-                                    Button() {
-                                        withAnimation {
-                                            isNewGame.toggle()
-                                        }
-                                    } label: {
-                                        Text("New Game")
-                                            .frame(maxWidth: .infinity)
-                                            .foregroundStyle(.white)
-                                    }
-                                    .listRowBackground(Color.clear)
-                                    .tint(.red)
-                                    .buttonStyle(.borderedProminent)
-                                    
-                                    Spacer()
-                                    Spacer()
-                                    
-                                    // To-Do: When games played = questions asked, disable button
-                                    Button() {
-                                        
-                                    } label: {
-                                        Text("Check Answer")
-                                            .frame(maxWidth: .infinity)
-                                            .foregroundStyle(.white)
-                                        
-                                    }
-                                    .listRowBackground(Color.clear)
-                                    .tint(.green)
-                                    .buttonStyle(.borderedProminent)
-                                }
-                            }
+                    if isGameOver {
+                        Text("Game Over. Your Final Score Is: \(playerScore)")
                             .listRowBackground(Color.clear)
-                        }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .foregroundStyle(.green)
                     }
                     
-                    // Debug section for selections to see what they are:
-                    Section(){
-                        Text("tt selected: \(timesTableSelected)")
-                        Text("tt multiplier: \(timesTableMultiplier)")
-                        Text("tt answer: \(timesTableMultiplier * timesTableSelected)")
-                        Text("questions to ask: \(Int(questionsToAsk))")
-                        Text("questions asked: \(questionsAsked)")
-                        Text("player score: \(playerScore)")
+                    if answerCorrectShow {
+                        if answerCorrect {
+                            Text("Correct")
+                                .listRowBackground(Color.clear)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .foregroundStyle(.green)
+                        } else {
+                            Text("Incorrect")
+                                .listRowBackground(Color.clear)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .foregroundStyle(.red)
+                        }
                     }
                 }
-                .navigationTitle("Times Tables")
                 
+                // Debug section for selections to see what they are:
+//                Section(){
+//                    Text("tt selected: \(timesTableSelected)")
+//                    Text("tt multiplier: \(timesTableMultiplier)")
+//                    Text("tt answer: \(timesTableMultiplier * timesTableSelected)")
+//                    Text("questions to ask: \(Int(questionsToAsk))")
+//                    Text("questions asked: \(questionsAsked)")
+//                    Text("player answer: \(playerAnswer)")
+//                    Text("player score: \(playerScore)")
+//                    Text("button disabled: \(String(answerButtonDisabled))")
+//                }
             }
-            .toolbar {
-                // Add button to hide the keyboard. Shows up in the top right.
-                if answerIsFocused {
-                    Button("Done") {
-                        answerIsFocused = false
-                    }
-                }
-            }
+            .navigationTitle("Times Tables")
             
         }
-        
-        
-        // 3. Randomly generate the question.
-        
-        
+        .toolbar {
+            // Add button to hide the keyboard. Shows up in the top right.
+            if answerIsFocused {
+                Button("Done") {
+                    answerIsFocused = false
+                }
+            }
+        }
+    }
+    
+    func randomNumber() -> Int {
+        return Int.random(in: 1..<13)
     }
     
     func checkAnswer() {
+        let answer = timesTableSelected * timesTableMultiplier
         
+        print("Button 2: \(answerButtonDisabled)")
+        
+        if answer == playerAnswer {
+            answerCorrect = true
+            answerCorrectShow = true
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                answerButtonDisabled = true
+                answerCorrect = false
+                answerCorrectShow = false
+                playerScore += 1
+                nextRound()
+            }
+            
+            
+        } else {
+            answerCorrect = false
+            answerCorrectShow = true
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                answerCorrect = false
+                answerCorrectShow = false
+                nextRound()
+            }
+        }
     }
+    
+    func nextRound() {
+        if Int(questionsToAsk) == questionsAsked {
+            answerButtonDisabled = true
+            isGameOver = true
+            
+        } else {
+            questionsAsked += 1
+            timesTableMultiplier = randomNumber()
+            
+            answerButtonDisabled = false
+            
+            print("Button 3: \(answerButtonDisabled)")
+        }
+    }
+    
+    
 }
 
 #Preview {
