dbd203fb096124c81d840efdc84695f44e6c0725
dev_neil_a <neilallwood@icloud.com>
2024-02-12 11:57:02 +0000

    Added sheet example

--- 020-project-007-iexpense/code-examples/code-examples/ContentView.swift
+++ 020-project-007-iexpense/code-examples/code-examples/ContentView.swift
@@ -2,6 +2,21 @@
 
 import SwiftUI
 
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss // This will be used to dismiss the sheet
+    
+    let name: String
+    
+    var body: some View {
+        VStack {
+            Text("Hello \(name)")
+            Button("Dismiss Sheet") {
+                dismiss()
+            }
+        }
+    }
+}
+
 @Observable
 class User {
     var firstName = "Bilbo"
@@ -9,6 +24,7 @@ class User {
 }
 
 struct ContentView: View {
+    @State public var showingSheet = false
     @State private var user = User()
 
     var body: some View {
@@ -17,6 +33,13 @@ struct ContentView: View {
 
             TextField("First name", text: $user.firstName)
             TextField("Last name", text: $user.lastName)
+            
+            Button("Show Sheet") {
+                            showingSheet.toggle()
+                        }
+                        .sheet(isPresented: $showingSheet) {
+                            SecondView(name: user.firstName)
+                        }
         }
     }
 }
