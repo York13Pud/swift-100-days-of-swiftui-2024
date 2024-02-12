5cda30e047534849d6e9bc6e082a42642f8e503c
dev_neil_a <neilallwood@icloud.com>
2024-02-12 12:30:26 +0000

    Added onDelete example

--- 020-project-007-iexpense/code-examples/code-examples/ContentView.swift
+++ 020-project-007-iexpense/code-examples/code-examples/ContentView.swift
@@ -27,20 +27,46 @@ struct ContentView: View {
     @State public var showingSheet = false
     @State private var user = User()
 
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     var body: some View {
         VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-            
-            Button("Show Sheet") {
-                            showingSheet.toggle()
-                        }
-                        .sheet(isPresented: $showingSheet) {
-                            SecondView(name: user.firstName)
-                        }
+            NavigationStack {
+                VStack {
+                    
+                    List {
+                        ForEach(numbers, id: \.self) {
+                            Text("Row \($0)")
+                        }.onDelete(perform: removeRows)
+                    }
+                    
+                    Button("Add Number To List") {
+                        numbers.append(currentNumber)
+                        currentNumber += 1
+                    }
+                    
+                }.navigationTitle("Testing")
+                    .toolbar {
+                        EditButton()
+                    }
+                
+                Text("Your name is \(user.firstName) \(user.lastName).")
+                
+                TextField("First name", text: $user.firstName)
+                TextField("Last name", text: $user.lastName)
+                
+                Button("Show Sheet") {
+                    showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SecondView(name: user.firstName)
+                }
+            }
         }
+    }
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
