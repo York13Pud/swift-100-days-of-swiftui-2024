59de4a40d337b9856ac8307c4ddb4221ddbcaf95
dev_neil_a <neilallwood@icloud.com>
2024-02-13 16:47:00 +0000

    Added id to ExpenseItem struct

--- 020-project-007-iexpense/iexpense/iexpense/ContentView.swift
+++ 020-project-007-iexpense/iexpense/iexpense/ContentView.swift
@@ -3,7 +3,8 @@
 import SwiftUI
 
 // Define a struct that has the properties for each expense:
-struct ExpenseItem {
+struct ExpenseItem: Identifiable {
+    let id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -23,11 +24,19 @@ struct ContentView: View {
         NavigationStack {
             // Create a list for the expenses:
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items, id: \.id) { item in
                     Text(item.name)
                 }
                 // Add a delete option to each list item:
                 .onDelete(perform: removeItems)
+                
+                // Another option to do the ForEach loop with a little less code.
+                // The loop will know that the list item has an identifiable id by default:
+                // ForEach(expenses.items) { item in
+                //     Text(item.name)
+                // }
+                // .onDelete(perform: removeItems)
+                
             }
             .navigationTitle("iExpense")
             // Add a button to the navigation toolbar to add an item to the list
