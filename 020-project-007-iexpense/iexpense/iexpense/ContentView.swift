1457f33bebbe03252417d239a0ce5fb9444fbde6
dev_neil_a <neilallwood@icloud.com>
2024-02-13 15:58:58 +0000

    Created simple list with addition and deletion of items

--- 020-project-007-iexpense/iexpense/iexpense/ContentView.swift
+++ 020-project-007-iexpense/iexpense/iexpense/ContentView.swift
@@ -2,15 +2,47 @@
 
 import SwiftUI
 
+// Define a struct that has the properties for each expense:
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+// Define a class with one property that has an array of ExpenseItems
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    // Create an instance of Expenses:
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            // Create a list for the expenses:
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                // Add a delete option to each list item:
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            // Add a button to the navigation toolbar to add an item to the list
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
         }
-        .padding()
+        
+    }
+    // Define a function to preform the deletion of an item in the list:
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
