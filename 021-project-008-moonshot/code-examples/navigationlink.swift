99ed414fd3e7451e022f798db484c10c47437888
dev_neil_a <neilallwood@icloud.com>
2024-02-19 19:36:59 +0000

    Added navigationlink example

--- 021-project-008-moonshot/code-examples/navigationlink.swift
+++ 021-project-008-moonshot/code-examples/navigationlink.swift
@@ -4,7 +4,18 @@ import SwiftUI
 
 struct navigationlink: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(0..<100) { row in
+                // NavigationLink will allow a list item to be shown with a > at the end
+                // to indicate that it will take the user to a new screen.
+                // This is the recommended way to do this from a UX perspective.
+
+                NavigationLink("Row \(row)") {
+                    Text("Detail \(row)")
+                }
+            }
+            .navigationTitle("SwiftUI")
+        }
     }
 }
 
