c25640c3720addd49bfb18429ac084bd3ff6cb5a
dev_neil_a <neilallwood@icloud.com>
2024-02-19 13:02:21 +0000

    Added LazyStack examples

--- 021-project-008-moonshot/code-examples/LazyStacks.swift
+++ 021-project-008-moonshot/code-examples/LazyStacks.swift
@@ -2,10 +2,47 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct LazyStacks: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // An example of a vertical LazyVStack:
+        ScrollView(.vertical) {
+            // A LazyVStack works the same as a VStack when in a ScrollView but it will only
+            // generate the scrollable content that is needed to fill the view.
+            // When you scroll, the additional data views are generated and shown.
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+        
+        // An example of a horizontal LazyVStack:
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+        }
     }
+    
+    
 }
 
 #Preview {
--- 021-project-008-moonshot/code-examples/code-examples/ContentView.swift
+++ 021-project-008-moonshot/code-examples/code-examples/ContentView.swift
@@ -4,13 +4,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // By default, all images are sized to their full size.
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("example") // Or you can use Image(.example)
+                .resizable() // Sizes the image to fit inside the frame.
+                .scaledToFit() // The image is scaled to fit but there could be space left.
+            // .scaledToFill() // This will scale the image to fit an aspect ratio. It will go outside the frame if needed.
+                .frame(width: 80, height: 100)
+            // .clipped() // Shows only the part of the image within the frame if resizable is not used.
+            
+            Image(.example)
+                .resizable()
+                .scaledToFit()
+                .containerRelativeFrame(.horizontal) { size, axis in
+                    size * 0.8 // Make the image fill 80% of the screen horizontally.
+                } // Creates a container frame that is relative to the screen size.
         }
-        .padding()
     }
 }
 
