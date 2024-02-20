ed5d0bcc2ba558f31926544b3962992eb660d9a6
dev_neil_a <neilallwood@icloud.com>
2024-02-20 17:03:51 +0000

    Updated code

--- 021-project-008-moonshot/moonshot/moonshot/Bundle-Decodable.swift
+++ 021-project-008-moonshot/moonshot/moonshot/Bundle-Decodable.swift
@@ -3,21 +3,29 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+    func decode<T: Codable>(_ file: String) -> T { // T is a generic in this case so the input data dan be anything.
+        // Attempts to locate the file containing the data:
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
-
+        // Attempts to load the file containing the data:
         guard let data = try? Data(contentsOf: url) else {
             fatalError("Failed to load \(file) from bundle.")
         }
-
+        
+        // Instantiate an instance of the JSON decoder:
         let decoder = JSONDecoder()
-
-        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+        
+        // Format the date:
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        // Lastly, process the JSON data into a dictionary:
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle.")
         }
-
+        
         return loaded
     }
 }
--- 021-project-008-moonshot/moonshot/moonshot/ContentView.swift
+++ 021-project-008-moonshot/moonshot/moonshot/ContentView.swift
@@ -3,14 +3,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Load the data in the astronauts.json file:
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let columns = [GridItem(.adaptive(minimum: 150))]
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                            .padding()
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
-        .padding()
+        
+        // Text(String(astronauts.count))
+        // Text(String(missions.count))
     }
 }
 
