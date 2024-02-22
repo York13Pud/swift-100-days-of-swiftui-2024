084e1acbb758db8ea53f92439f8c0ece41df04ec
dev_neil_a <neilallwood@icloud.com>
2024-02-22 15:18:42 +0000

    Updated code and added additional views

---
+++ 021-project-008-moonshot/moonshot/moonshot/MissionView.swift
@@ -0,0 +1,13 @@
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
