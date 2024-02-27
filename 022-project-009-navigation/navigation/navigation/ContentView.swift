3527a517b43eace0f371f0080c7b1d24f6564edc
dev_neil_a <neilallwood@icloud.com>
2024-02-27 16:05:05 +0000

    Added examples

--- 022-project-009-navigation/navigation/navigation/ContentView.swift
+++ 022-project-009-navigation/navigation/navigation/ContentView.swift
@@ -1,16 +1,48 @@
-//
+// This is programatic UI navigation.
+// Essentially this means that we don't have to wait for the user to press a button
+// to progress.
 
 import SwiftUI
 
 struct ContentView: View {
+    // NavigationPath allows for the storage of any datatype, rather than just one.
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // Bind navigation stack to path as a two-way binding.
+        NavigationStack(path: $path) {
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+
+                ForEach(0..<5) { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            // Use these methods only when the datatype is specified:
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected the number \(selection)")
+            }
+            
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
+            }
+            .toolbar {
+                // This will set path to have an int of 556:
+                Button("Push 556") {
+                    path.append(556)
+                }
+                
+                Spacer()
+                
+                // This will set path to have a sreing of hello:
+                Button("Push Hello") {
+                    path.append("Hello")
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
