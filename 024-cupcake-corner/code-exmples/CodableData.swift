9ae6332ad4a9a151b58b2bc628429dc0ed31e69c
dev_neil_a <neilallwood@icloud.com>
2024-03-18 19:57:42 +0000

    Added code examples

---
+++ 024-cupcake-corner/code-exmples/CodableData.swift
@@ -0,0 +1,51 @@
+//
+
+import SwiftUI
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct codable_data: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+                }
+            }.task {
+                await loadData()
+            }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=bon+jovi&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+            
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
+#Preview {
+    codable_data()
+}
