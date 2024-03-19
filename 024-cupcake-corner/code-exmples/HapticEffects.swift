31164cd92bb55065f59a8216507e6311aeb1c727
dev_neil_a <neilallwood@icloud.com>
2024-03-19 15:32:20 +0000

    Added haptic examples

--- 024-cupcake-corner/code-exmples/HapticEffects.swift
+++ 024-cupcake-corner/code-exmples/HapticEffects.swift
@@ -3,8 +3,19 @@
 import SwiftUI
 
 struct HapticEffects: View {
+    @State private var counter = 0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Tap Count: \(counter)") {
+            counter += 1
+        }
+        // Use haptic feedback to run when the button is tapped and increase
+        // based on the counter:
+        .sensoryFeedback(.increase, trigger: counter)
+
+        // Other examples of using the haptic feedback engine:
+        // .sensoryFeedback(.impact(flexibility: .soft, intensity: 0.5), trigger: counter)
+        // .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
     }
 }
 
