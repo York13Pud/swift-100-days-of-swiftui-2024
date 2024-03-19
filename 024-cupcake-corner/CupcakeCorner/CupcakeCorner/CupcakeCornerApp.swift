f60ba5f9b8ff364c8b62cb520e98d43b5f7bcf7c
dev_neil_a <neilallwood@icloud.com>
2024-03-19 16:13:03 +0000

    Added example

---
+++ 024-cupcake-corner/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 024-cupcake-corner/code-exmples/CodableObservable.swift
@@ -0,0 +1,13 @@
+//
+
+import SwiftUI
+
+struct CodableObservable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodableObservable()
+}
