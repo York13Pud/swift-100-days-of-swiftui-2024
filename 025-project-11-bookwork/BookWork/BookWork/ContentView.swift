f137821aa1195ac6437ea4542dd471e09f4a47dd
dev_neil_a <neilallwood@icloud.com>
2024-04-01 15:22:27 +0100

    Added code to show a list of added books

--- 025-project-11-bookwork/BookWork/BookWork/ContentView.swift
+++ 025-project-11-bookwork/BookWork/BookWork/ContentView.swift
@@ -4,24 +4,41 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
+
     @Query var books: [Book]
 
     @State private var showingAddScreen = false
     
     var body: some View {
         NavigationStack {
-           Text("Count: \(books.count)")
-               .navigationTitle("Bookworm")
-               .toolbar {
-                   ToolbarItem(placement: .topBarTrailing) {
-                       Button("Add Book", systemImage: "plus") {
-                           showingAddScreen.toggle()
-                       }
+            List {
+                ForEach(books) { book in
+                    NavigationLink(value: book) {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+
+                            VStack(alignment: .leading) {
+                                Text(book.title)
+                                    .font(.headline)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+           .navigationTitle("Bookworm")
+           .toolbar {
+               ToolbarItem(placement: .topBarTrailing) {
+                   Button("Add Book", systemImage: "plus") {
+                       showingAddScreen.toggle()
                    }
                }
-               .sheet(isPresented: $showingAddScreen) {
-                   AddBookView()
-               }
+           }
+           .sheet(isPresented: $showingAddScreen) {
+               AddBookView()
+           }
        }
     }
 }
