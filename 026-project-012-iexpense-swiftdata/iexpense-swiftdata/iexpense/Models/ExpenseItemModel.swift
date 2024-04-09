c27ef21f64a583d0cc5d571beb87e39d1c275f1d
dev_neil_a <neilallwood@icloud.com>
2024-04-09 10:26:59 +0100

    Created model for expense items to use

--- 026-project-012-iexpense-swiftdata/iexpense-swiftdata/iexpense/Models/ExpenseItemModel.swift
+++ 026-project-012-iexpense-swiftdata/iexpense-swiftdata/iexpense/Models/ExpenseItemModel.swift
@@ -1,3 +1,21 @@
 //
 
 import Foundation
+import SwiftData
+
+// Define the model that will be used for an expense item:
+
+@Model
+class ExpenseItem {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+    
+    init(id: UUID = UUID(), name: String, type: String, amount: Double) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.amount = amount
+    }
+}
