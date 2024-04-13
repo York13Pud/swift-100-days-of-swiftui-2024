47b0b1bd665b7458b41d75133240921764b89c2e
dev_neil_a <neilallwood@icloud.com>
2024-04-13 20:45:52 +0100

    Added User and Friend models

--- 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Models/FriendModel.swift
+++ 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Models/FriendModel.swift
@@ -3,3 +3,13 @@
 import Foundation
 import SwiftData
 
+@Model
+class Friend {
+    var id: UUID
+    var name: String
+    
+    init(id: UUID, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
--- 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Models/UserModel.swift
+++ 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Models/UserModel.swift
@@ -4,3 +4,29 @@ import Foundation
 import SwiftData
 
 
+@Model
+class User {
+    var id: UUID
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date.ISO8601FormatStyle
+    var friends = [Friend]()
+    
+    init(id: UUID, isActive: Bool, name: String, age: Int, company: String, email: String, address: String, about: String, registered: Date.ISO8601FormatStyle, friends: [Friend?]) {
+        self.id = id
+        self.isActive = isActive
+        self.name = name
+        self.age = age
+        self.company = company
+        self.email = email
+        self.address = address
+        self.about = about
+        self.registered = registered
+    }
+    
+}
