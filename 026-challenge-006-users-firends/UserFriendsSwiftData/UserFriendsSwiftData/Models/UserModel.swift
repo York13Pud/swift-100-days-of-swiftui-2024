80034ee925d9874650c1ac42e440fb109d7b3f13
dev_neil_a <neilallwood@icloud.com>
2024-04-13 21:07:16 +0100

    Modified data property

--- 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Models/UserModel.swift
+++ 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Models/UserModel.swift
@@ -14,10 +14,10 @@ class User {
     var email: String
     var address: String
     var about: String
-    var registered: Date.ISO8601FormatStyle
+    var registered: Date
     var friends = [Friend]()
     
-    init(id: UUID, isActive: Bool, name: String, age: Int, company: String, email: String, address: String, about: String, registered: Date.ISO8601FormatStyle, friends: [Friend?]) {
+    init(id: UUID, isActive: Bool, name: String, age: Int, company: String, email: String, address: String, about: String, registered: Date, friends: [Friend?]) {
         self.id = id
         self.isActive = isActive
         self.name = name
