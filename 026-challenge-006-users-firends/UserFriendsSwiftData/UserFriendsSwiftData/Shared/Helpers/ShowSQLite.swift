d451767dc7391efe7cd59c9bbfe03cf9ab135e21
dev_neil_a <neilallwood@icloud.com>
2024-04-13 21:09:58 +0100

    Added extension to show SQLite database path for debugging

--- 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Shared/Helpers/ShowSQLite.swift
+++ 026-challenge-006-users-firends/UserFriendsSwiftData/UserFriendsSwiftData/Shared/Helpers/ShowSQLite.swift
@@ -1,3 +1,17 @@
 //
 
 import Foundation
+import SwiftData
+
+// Taken from:
+// https://www.hackingwithswift.com/quick-start/swiftdata/how-to-read-the-contents-of-a-swiftdata-database-store
+
+extension ModelContext {
+    var sqliteCommand: String {
+        if let url = container.configurations.first?.url.path(percentEncoded: false) {
+            "sqlite3 \"\(url)\""
+        } else {
+            "No SQLite database found."
+        }
+    }
+}
