917d6351829fc524e1fff8b0909db310f29b1143
dev_neil_a <neilallwood@icloud.com>
2024-05-03 12:03:28 +0100

    Renamed file

---
+++ 027-instafilter/CodeExamples/CodeExamples/ImageSwiftUI.swift
@@ -0,0 +1,24 @@
+//
+
+import SwiftUI
+
+struct CoreImageSwiftUI: View {
+    @State private var image: Image?
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        image = Image(.example)
+    }
+}
+
+#Preview {
+    CoreImageSwiftUI()
+}
