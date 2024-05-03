dff2c8389c6ef37465f69c1306fcbd2e0c1347dc
dev_neil_a <neilallwood@icloud.com>
2024-05-03 15:40:25 +0100

    Added example for ContentUnavailableView

---
+++ 027-instafilter/CodeExamples/CodeExamples/NoContent.swift
@@ -0,0 +1,22 @@
+//
+
+import SwiftUI
+
+struct NoContent: View {
+    var body: some View {
+        ContentUnavailableView {
+            Label("No snippets", systemImage: "swift")
+        } description: {
+            Text("You don't have any saved snippets yet.")
+        } actions: {
+            Button("Create Snippet") {
+                // create a snippet
+            }
+            .buttonStyle(.borderedProminent)
+        }
+    }
+}
+
+#Preview {
+    NoContent()
+}
