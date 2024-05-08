7bbb57fa8e3592bb15e6504888446655d0f3510d
dev_neil_a <neilallwood@icloud.com>
2024-05-08 13:34:00 +0100

    Updated app

--- 028-bucketlist/BucketList/BucketList/Models/LocationModel.swift
+++ 028-bucketlist/BucketList/BucketList/Models/LocationModel.swift
@@ -1,15 +1,27 @@
 //
 
 import Foundation
+import MapKit
 
 // Identifiable: so we can create many location markers in our map.
 // Codable: so we can load and save map data easily.
 // Equatable, so we can find one particular location in an array of locations.
 
 struct Location: Codable, Equatable, Identifiable {
-    let id: UUID
+    var id: UUID
     var name: String
     var description: String
     var latitude: Double
     var longitude: Double
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: latitude,
+            longitude: longitude
+        )}
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
--- 028-bucketlist/BucketList/BucketList/Views/ContentView.swift
+++ 028-bucketlist/BucketList/BucketList/Views/ContentView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
     
     let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
@@ -16,9 +17,19 @@ struct ContentView: View {
         // Use MapReader to get the map co-ords, rather than the screen co-ords:
         MapReader { proxy in
             Map(initialPosition: startPosition) {
-                // Show each of the markers in the locations array on the map:
+                // Show each of the annotations / markers in the locations array on the map:
                 ForEach(locations) { location in
-                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
+                    }
                 }
             }
             // When a location on the map is tapped, create a location marker:
@@ -28,7 +39,15 @@ struct ContentView: View {
                     locations.append(newLocation)
                     }
                 }
+            
             .mapStyle(.hybrid)
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
+                }
+            }
         }
     }
 }
